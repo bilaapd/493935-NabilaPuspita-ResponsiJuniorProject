cf2caee4c64a2e2e91f647bf17799818ffbd620f
bilaapd <114081903+bilaapd@users.noreply.github.com>
2024-12-02 09:44:33 +0700

    fix : commit

---
+++ Responsijunpro/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace Responsijunpro
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            cbDep = new ComboBox();
+            lbDep = new ListBox();
+            btnInsert = new Button();
+            btnEdit = new Button();
+            btnDelete = new Button();
+            dataGridView1 = new DataGridView();
+            lblLogo = new Label();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(25, 124);
+            label1.Name = "label1";
+            label1.Size = new Size(99, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Nama Karyawan :";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(25, 170);
+            label2.Name = "label2";
+            label2.Size = new Size(97, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Dep. Karyawan   :";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(177, 124);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(160, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // cbDep
+            // 
+            cbDep.FormattingEnabled = true;
+            cbDep.Items.AddRange(new object[] { "HR", "ENG", "DEV", "PM", "FIN" });
+            cbDep.Location = new Point(177, 162);
+            cbDep.Name = "cbDep";
+            cbDep.Size = new Size(160, 23);
+            cbDep.TabIndex = 4;
+            // 
+            // lbDep
+            // 
+            lbDep.FormattingEnabled = true;
+            lbDep.ItemHeight = 15;
+            lbDep.Items.AddRange(new object[] { "HR : Human Resource", "ENG : Engineer", "DEV : Developer", "PM : Product Manager", "FIN : Finance" });
+            lbDep.Location = new Point(377, 16);
+            lbDep.Name = "lbDep";
+            lbDep.Size = new Size(155, 169);
+            lbDep.TabIndex = 5;
+            // 
+            // btnInsert
+            // 
+            btnInsert.Location = new Point(28, 219);
+            btnInsert.Name = "btnInsert";
+            btnInsert.Size = new Size(93, 26);
+            btnInsert.TabIndex = 6;
+            btnInsert.Text = "Insert";
+            btnInsert.UseVisualStyleBackColor = true;
+            btnInsert.Click += btnInsert_Click_1;
+            // 
+            // btnEdit
+            // 
+            btnEdit.Location = new Point(235, 219);
+            btnEdit.Name = "btnEdit";
+            btnEdit.Size = new Size(93, 26);
+            btnEdit.TabIndex = 7;
+            btnEdit.Text = "Edit";
+            btnEdit.UseVisualStyleBackColor = true;
+            btnEdit.Click += btnEdit_Click_1;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Location = new Point(439, 219);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(93, 26);
+            btnDelete.TabIndex = 8;
+            btnDelete.Text = "Delete";
+            btnDelete.UseVisualStyleBackColor = true;
+            btnDelete.Click += btnDelete_Click_1;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(28, 266);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(505, 269);
+            dataGridView1.TabIndex = 9;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick_1;
+            // 
+            // lblLogo
+            // 
+            lblLogo.AutoSize = true;
+            lblLogo.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            lblLogo.Location = new Point(108, 16);
+            lblLogo.Name = "lblLogo";
+            lblLogo.Size = new Size(251, 21);
+            lblLogo.TabIndex = 10;
+            lblLogo.Text = "Employee Database Management";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackgroundImage = (Image)resources.GetObject("pictureBox1.BackgroundImage");
+            pictureBox1.Location = new Point(28, 16);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(65, 66);
+            pictureBox1.TabIndex = 11;
+            pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(585, 557);
+            Controls.Add(pictureBox1);
+            Controls.Add(lblLogo);
+            Controls.Add(dataGridView1);
+            Controls.Add(btnDelete);
+            Controls.Add(btnEdit);
+            Controls.Add(btnInsert);
+            Controls.Add(lbDep);
+            Controls.Add(cbDep);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private ComboBox cbDep;
+        private ListBox lbDep;
+        private Button btnInsert;
+        private Button btnEdit;
+        private Button btnDelete;
+        private DataGridView dataGridView1;
+        private Label lblLogo;
+        private PictureBox pictureBox1;
+    }
+}
